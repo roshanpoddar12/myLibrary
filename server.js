0f210c028880fe033003cbc21188aa1c2a316050
Roshan <you@example.com>
2020-05-21 19:49:54 +0530

    setup

---
+++ server.js
@@ -0,0 +1,24 @@
+ const dotenv = require('dotenv');
+ dotenv.config({ path: './.env' });
+ const express = require('express');
+ const app = express();
+ const expressLayouts = require('express-ejs-layouts');
+ const mongoose = require('mongoose');
+ const indexRouter = require('./routes/index');
+
+ app.set('view engine','ejs');
+ app.set('views',__dirname+'/views');
+ app.set('layout','layouts/layout');
+ app.use(expressLayouts);
+ app.use(express.static('public'));
+ mongoose
+ .connect(process.env.DATABASE_URL, {
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useFindAndModify: false
+ })
+ .then(() => console.log('Connected to the Database'));
+
+ app.use('/',indexRouter);
+
+ app.listen(process.env.PORT || 3000)
